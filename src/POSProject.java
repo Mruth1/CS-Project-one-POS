e0cfa5a72e925f3a543c6acd20c9c0a646d25ccf
Marcus <marcusruth2003@gmail.com>
2024-09-20 18:54:58 -0400

    Added 2 new fields to the main POS GUI

--- src/POSProject.java
+++ src/POSProject.java
@@ -3,31 +3,50 @@
 /// that uses make pricing for items created within the code, have a java swing gui to navigate through the system and will calculate the total cost with taxes based on the state I reside in (FL)
 /// With this being said, my code to whomever see's this isnt perfect, but its a swing (no pun intended) at my first project.
 Date Started: 9/19/24 */
-
-
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class POSProject {
+    //creating a method that will hold all the buttons to access POS options
+    static void mainWindow() {
+        JFrame mainframe = new JFrame("POS System"); // the window name
+        mainframe = new JFrame("POS System"); // sets the title of the main window
+        mainframe.pack();
+        mainframe.setSize(2560, 1440); //resolution of my main monitor
+        mainframe.setVisible(true);
+
+        JPanel classicsPanel = classicsPanel(); //pulls "classicsPanel" from classicsPanel method
+        mainframe.add(classicsPanel, BorderLayout.CENTER);
+        JPanel totalCostPanel = totalCost();
+        mainframe.add(totalCostPanel, BorderLayout.EAST);
+    }
+    //creates a useable button for the "Classic" Food option within the POS
+    static JPanel classicsPanel() {
+        JPanel classicsPanel = new JPanel();
+        classicsPanel.setBorder(BorderFactory.createTitledBorder("Classics"));
+        classicsPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        classicsPanel.setLayout(new BoxLayout(classicsPanel, BoxLayout.Y_AXIS));
+        classicsPanel.setVisible(true);
+        JButton classicsItemButton = new JButton("Classics");
+        classicsPanel.add(classicsItemButton);
+        return classicsPanel;
+    }
+    static JPanel totalCost() {
+        JPanel totalCostPanel = new JPanel();
+        totalCostPanel.setBorder(BorderFactory.createTitledBorder("Total Cost"));
+        totalCostPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        totalCostPanel.setLayout(new BoxLayout(totalCostPanel, BoxLayout.Y_AXIS));
+        totalCostPanel.setVisible(true);
+        JButton totalCostItemButton = new JButton("Total Cost"); // this needs to be changed to be not be a button, more a less a window, within the window
+        totalCostPanel.add(totalCostItemButton);
+        return totalCostPanel;
+    }
+
+
     public static void main(String[] args) {
-        // create a JFRAME and set the properties of it
-        JFrame frame = new JFrame("Gui Testing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-        int[] amountOfClicks = {0};
-        JButton button = new JButton("Press: "+amountOfClicks) ;
-        frame.getContentPane().add(button); // adds button to content pane of frame
-
-        button.addActionListener(new ActionListener() { // Action Listener respondes to the button when it is clicked
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                amountOfClicks[0]++;
-
-                button.setText("Press: "+amountOfClicks[0]) ;
-            }
-        });
-
+    mainWindow();
     }
 }
