18ac2fcf9068becfa1aab5b4ba5ae3d89f481b54
Marcus <marcusruth2003@gmail.com>
2024-09-21 15:14:24 -0400

    + New Source File - redid some of the code to be slightly more dynamic

---
+++ src/Food.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Food { // file to have all the food options in
+        //creates method for a specific food button in POSProject.java
+        public JButton createFoodButton(String foodName) {
+            JButton createFoodButton = new JButton(foodName);
+             // action listeners here for what each button made should do (DRY Principle)
+            return createFoodButton;
+
+    }
+}
+
+
+// create action listeners that add food totals to total cost and display the price on the left along with the food item name.
+// create a menu once a button is clicked to add the food into
+// get math to correspond correctly
+// most likely move total cost to a seperate source file
+// rest to be done is TBD
--- src/POSProject.java
+++ src/POSProject.java
@@ -9,40 +9,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
+
 public class POSProject {
     //creating a method that will hold all the buttons to access POS options
     static void mainWindow() {
         JFrame mainframe = new JFrame("POS System"); // the window name
-        mainframe = new JFrame("POS System"); // sets the title of the main window
-        mainframe.pack();
+        mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainframe.setSize(2560, 1440); //resolution of my main monitor
         mainframe.setVisible(true);
+        JPanel totalCostPanel = totalCost(); // creates total costpanel to mainWindow
+        mainframe.add(totalCostPanel, BorderLayout.EAST); // sets the totalcostPanel to the right side of the GUI
 
-        JPanel classicsPanel = classicsPanel(); //pulls "classicsPanel" from classicsPanel method
-        mainframe.add(classicsPanel, BorderLayout.CENTER);
-        JPanel totalCostPanel = totalCost();
-        mainframe.add(totalCostPanel, BorderLayout.EAST);
+        // create buttons with Food File
+        Food foodButton = new Food();
+        JButton classicsFood = foodButton.createFoodButton("Classics");
+        JButton uniqueFood = foodButton.createFoodButton("Unique Food");
+
+        //create a panel for foods to be in
+        JPanel foodPanel = new JPanel();
+        foodPanel.setBorder(BorderFactory.createTitledBorder("Food List"));
+        foodPanel.add(classicsFood);
+        foodPanel.add(uniqueFood);
+
+        mainframe.add(foodPanel, BorderLayout.CENTER);
+
     }
-    //creates a useable button for the "Classic" Food option within the POS
-    static JPanel classicsPanel() {
-        JPanel classicsPanel = new JPanel();
-        classicsPanel.setBorder(BorderFactory.createTitledBorder("Classics"));
-        classicsPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-        classicsPanel.setLayout(new BoxLayout(classicsPanel, BoxLayout.Y_AXIS));
-        classicsPanel.setVisible(true);
-        JButton classicsItemButton = new JButton("Classics");
-        classicsPanel.add(classicsItemButton);
-        return classicsPanel;
-    }
+
+    // create a window displaying the total cost, this will have a subtotal and implement the state tax in florida as two seperate values. then adds the both to show "amount due"
     static JPanel totalCost() {
-        JPanel totalCostPanel = new JPanel();
-        totalCostPanel.setBorder(BorderFactory.createTitledBorder("Total Cost"));
-        totalCostPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-        totalCostPanel.setLayout(new BoxLayout(totalCostPanel, BoxLayout.Y_AXIS));
-        totalCostPanel.setVisible(true);
-        JButton totalCostItemButton = new JButton("Total Cost"); // this needs to be changed to be not be a button, more a less a window, within the window
-        totalCostPanel.add(totalCostItemButton);
-        return totalCostPanel;
+        //create variables to do increment when POS is being used
+        double subtotalCost = 0.00;
+        double taxCost = 0.00;
+        double totalDue = 0.00; // wanted to do amount do but interferes with JLabel within this method
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createTitledBorder("Total Cost"));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // labels within the panel, sets the cost to only 2 decimal places
+        JLabel subtotalLabel = new JLabel(String.format("Subtotal: $%.2f", subtotalCost));
+        JLabel taxLabel = new JLabel(String.format("Tax: $%.2f", taxCost));
+        JLabel amountDue = new JLabel(String.format("Amount Due: $%.2f", totalDue));
+
+        //aligns the numbers at the bottom right of the screen
+
+        panel.add(subtotalLabel);
+        panel.add(taxLabel);
+        panel.add(amountDue);
+
+        return panel;
     }
 
 
